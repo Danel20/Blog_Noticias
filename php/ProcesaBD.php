13e2479a3f42559a7cb05f3c203663a18b33d11c
Danel20 <96256396+Danel20@users.noreply.github.com>
2023-10-10 14:35:11 -0600

    ProcesaBD.php

--- php/ProcesaBD.php
+++ php/ProcesaBD.php
@@ -4,28 +4,10 @@ $NombreBD='Blog_Noticias';
 $NombreTabla='Noticias';
 $ConexionSQL = mysqli_connect('localhost','root','');
 
-
-/*CREAR CONEXIÓN*/
-function CrearConexion($a1){
-/*Comprobar Conexión...
-if($a1) {
-	echo 'Conexión Creada<br>';
-} else {
-	echo 'Error Conectando...<br>';
-};
-*/
-};
-
 /* CREAR BASE DE DATOS */
 function CrearBD($a1,$a2){
 $Operacion='CREATE DATABASE IF NOT EXISTS '.$a2;
-/*Comprobar Conexión...
-if(mysqli_query($a1, $Operacion)) {
-	echo 'Base de Datos: -'.$a2.'- Creada.<br>';
-} else {
-	echo 'Error Creando Base de Datos...<br>';
-};
-*/
+mysqli_query($a1, $Operacion);
 };
 
 /* CREAR TABLA */
@@ -33,13 +15,7 @@ function CrearTabla($a1,$a2){
 $Operacion='CREATE TABLE IF NOT EXISTS '.$a2.'(
 title VARCHAR(65) NOT NULL,
 body TEXT(3000) NOT NULL)';
-/*Comprobar Operacion...
-if(mysqli_query($a1, $Operacion)) {
-	echo 'Tabla -'.$a2.'- Creada<br>';
-} else {
-	echo 'Error Creando Tabla...<br>';
-};
-*/
+mysqli_query($a1, $Operacion);
 };
 
 /*MOSTRAR REGISTROS*/
@@ -54,16 +30,8 @@ while($Recorrido = mysqli_fetch_array($Resultado)){
 /* DESCONEXIÓN */
 function DesconectarConexion($a1){
 $DesconectarseASQL = mysqli_close($a1);
-/*Comprobar Desconexión...
-if($a1) {
-	echo 'Desconexión realizada<br>';
-} else {
-	echo 'Error Desconectando...<br>';
-};
-*/
 };
 
-CrearConexion($ConexionSQL);
 CrearBD($ConexionSQL,$NombreBD);
 $ConectarseASQL = mysqli_connect('localhost','root','',$NombreBD);
 CrearTabla($ConectarseASQL,$NombreTabla);
