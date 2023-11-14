888ca604999d592316c9b583e63d9587ed3892f7
Danel20 <96256396+Danel20@users.noreply.github.com>
2023-11-13 21:17:50 -0600

    Update view.php

--- php/view.php
+++ php/view.php
@@ -5,10 +5,15 @@ $NombreBD='Blog_Noticias';
 $NombreTabla='Noticias';
 $ConexionSQL = mysqli_connect('localhost','root','');
 
+function MostrarRegistrosJSON($a1,$a2){
+header("Content-Type: application/json");
+echo json_encode(SacarListaDeRegistros($a1,$a2));
+};
+
 CrearConexion($ConexionSQL);
 CrearBD($ConexionSQL,$NombreBD);
 $ConectarseASQL = mysqli_connect('localhost','root','',$NombreBD);
 CrearTabla($ConectarseASQL,$NombreTabla);
-MostrarRegistros($ConectarseASQL,$NombreTabla);
+MostrarRegistrosJSON($ConectarseASQL,$NombreTabla);
 DesconectarConexion($ConectarseASQL);
 ?>
