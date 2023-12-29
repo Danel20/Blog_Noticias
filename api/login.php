a44fe0e6de905acee59995106d07415a4251279e
Danel20 <96256396+Danel20@users.noreply.github.com>
2023-12-28 22:55:16 -0600

    Add files via upload

--- api/login.php
+++ api/login.php
@@ -2,13 +2,15 @@
 include "../php/models.php";
 /*VARIABLES*/
 $NombreBD='Blog_Noticias';
-$NombreTabla='Noticias';
+$NombreTabla='Users';
 $ConexionSQL = mysqli_connect('localhost','root','');
 
 CrearConexion($ConexionSQL);
 CrearBD($ConexionSQL,$NombreBD);
 $ConectarseASQL = mysqli_connect('localhost','root','',$NombreBD);
-CrearTabla($ConectarseASQL,$NombreTabla,'(title VARCHAR(65) NOT NULL,body TEXT(3000) NOT NULL)');
-MostrarRegistrosJSON($ConectarseASQL,$NombreTabla);
+CrearTabla($ConectarseASQL,$NombreTabla,'(name CHAR(25) NOT NULL,username CHAR(25) NOT NULL,password CHAR(200) NOT NULL,token CHAR(200) NOT NULL)');
+
+ComprobarLoginUsernamePass($_POST['username'],$_POST['password']);
+
 DesconectarConexion($ConectarseASQL);
 ?>
