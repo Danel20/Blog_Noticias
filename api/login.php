d5f4eafa0bdd652507de081a36f92aa410bc5285
Danel20 <96256396+Danel20@users.noreply.github.com>
2024-01-06 14:55:40 -0600

    Update login.php

--- api/login.php
+++ api/login.php
@@ -6,11 +6,7 @@ $NombreTabla='Users';
 $ConexionSQL = mysqli_connect('localhost','root','');
 
 CrearConexion($ConexionSQL);
-CrearBD($ConexionSQL,$NombreBD);
 $ConectarseASQL = mysqli_connect('localhost','root','',$NombreBD);
-CrearTabla($ConectarseASQL,$NombreTabla,'(name CHAR(25) NOT NULL,username CHAR(25) NOT NULL,password CHAR(200) NOT NULL,token CHAR(200) NOT NULL)');
-
 ComprobarLoginUsernamePass($_POST['username'],$_POST['password']);
-
 DesconectarConexion($ConectarseASQL);
 ?>
